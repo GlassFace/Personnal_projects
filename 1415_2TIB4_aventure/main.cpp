fbcf3cf5ed87bf535621b24ad529cb9669291022
Maxime Samyn <maxime.samyn@etud.infographie-sup.eu>
2014-10-03 01:09:01 +0200

    First commit -> aventure

---
+++ 1415_2TIB4_aventure/main.cpp
@@ -0,0 +1,26 @@
+#include "flib.h"
+
+
+
+void Initialize()
+{
+
+}
+
+
+void Update()
+{
+
+}
+
+
+void Render()
+{
+
+}
+
+
+void GfxMain(int, char *[])
+{
+	GfxCallbacks(Initialize, Update, Render);
+}
