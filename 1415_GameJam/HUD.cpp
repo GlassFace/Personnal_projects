2b8c03a120df3e6f391cc862d41061fab872bdc0
Crizz92 <kevin.henning92@yahoo.com>
2015-05-09 15:03:31 +0200

    User interface OK

--- 1415_GameJam/HUD.cpp
+++ 1415_GameJam/HUD.cpp
@@ -11,11 +11,17 @@
 #include "HUD.h"
 #include "Building.h"
 #include "House.h"
-
+#include <stdio.h>
 
 namespace
 {
 	const char * const MOUSE_TEXTURE_NAME = "Mouse.tga";
+
+	const char * const ALIVE_VILLAGER = "HUD\\VillagerHead.tga";
+	const char * const DEAD_VILLAGER = "HUD\\Skull.tga";
+	const char * const NUMBER = "HUD\\FigureSet.tga";
+	const char * const HUD_COLOR = "HUD\\HUDJauge.tga";
+	const char * const JAUGE_COLOR = "HUD\\JaugeColor.tga";
 
 	const float SUICIDE_GAUGE_SIZE_X_MAX = 300.f;
 	const float SUICIDE_GAUGE_SIZE_Y = 20.f;
@@ -23,6 +29,7 @@ namespace
 	const float SUICIDE_GAUGE_POS_Y = 100.f;
 	const float SUICIDE_GAUGE_SPEED = 3.f;
 	const float SUICIDE_MALUS = 10.f;
+	const float SUICIDE_MALUS_MAX = 300.f;
 
 	const float VILLAGER_COUNTER_POS_X = 100.f;
 	const float VILLAGER_COUNTER_POS_Y = 100.f;
@@ -36,6 +43,23 @@ TSuicideInfo THUD::s_tSuicideInfo;
 
 TGfxTexture * THUD::s_pMouseTexture = nullptr;
 TGfxSprite * THUD::s_pMouseSprite = nullptr;
+
+TGfxTexture * THUD::s_pDigitText = nullptr;
+
+TGfxTexture * THUD::s_pVillagerAliveTexture = nullptr;
+TGfxSprite * THUD::s_pVillagerAlive = nullptr;
+TGfxSprite ** THUD::s_pVillagerAliveText = nullptr;
+
+TGfxTexture * THUD::s_pVillagerDeadTexture = nullptr;
+TGfxSprite * THUD::s_pVillagerDead = nullptr;
+TGfxSprite ** THUD::s_pVillagerDeadText = nullptr;
+
+
+TGfxTexture * THUD::s_pSuicidometreTex = nullptr;
+TGfxTexture * THUD::s_pSuicidometreDecoTex = nullptr;
+
+TGfxSprite * THUD::s_pSuicidometre = nullptr;
+TGfxSprite * THUD::s_pSuicidometreDeco = nullptr;
 
 THUD::THUD()
 {
@@ -56,10 +80,53 @@ void THUD::S_Initialize()
 	GfxSpriteSetPivot(s_pMouseSprite, 10.0f, 10.0f);
 	GfxSpriteSetPosition(s_pMouseSprite, float(GfxGetCurrentMouseX()), float(GfxGetCurrentMouseY()));
 
+	// Initialize villager alive counter;
+
+
+	// ALIVE SPRITE
+	s_pVillagerAliveTexture = GfxTextureLoad(ALIVE_VILLAGER);
+	s_pVillagerAlive = GfxSpriteCreate(s_pVillagerAliveTexture);
+	GfxSpriteSetPivot(s_pVillagerAlive, 32.f, 32.f);
+	GfxSpriteSetPosition(s_pVillagerAlive, 100.f, 100.f);
+	
+	s_pDigitText = GfxTextureLoad(NUMBER);
+
+	s_pVillagerAliveText = new TGfxSprite *[NBR_SPRITE]{ 0 };
+	for (int i = 0; i < NBR_SPRITE; i++)
+	{
+		s_pVillagerAliveText[i] = GfxSpriteCreate(s_pDigitText);
+		GfxSpriteSetPivot(s_pVillagerAliveText[i], 15, 30);
+		GfxSpriteSetPosition(s_pVillagerAliveText[i], 164.f + i * 30, 100.f);
+	}
+
+
+	// DEAD SPRITE
+	s_pVillagerDeadTexture = GfxTextureLoad(DEAD_VILLAGER);
+	s_pVillagerDead = GfxSpriteCreate(s_pVillagerDeadTexture);
+	GfxSpriteSetPivot(s_pVillagerDead, 32.f, 32.f);
+	GfxSpriteSetPosition(s_pVillagerDead, 100.f, 164.f);
+	s_pVillagerDeadText = new TGfxSprite *[NBR_SPRITE]{ 0 };
+	for (int i = 0; i < NBR_SPRITE; i++)
+	{
+		s_pVillagerDeadText[i] = GfxSpriteCreate(s_pDigitText);
+		GfxSpriteSetPivot(s_pVillagerDeadText[i], 15, 30);
+		GfxSpriteSetPosition(s_pVillagerDeadText[i], 164.f + i * 30, 164.f);
+	}
+
+
+
+	// SUICIDOMETRE
+	s_pSuicidometreTex = GfxTextureLoad(JAUGE_COLOR);
+	s_pSuicidometreDecoTex = GfxTextureLoad(HUD_COLOR);
+	s_pSuicidometre = GfxSpriteCreate(s_pSuicidometreTex);
+	s_pSuicidometreDeco = GfxSpriteCreate(s_pSuicidometreDecoTex);
+
+	GfxSpriteSetPosition(s_pSuicidometre, 1320.f, 50.f);
+	GfxSpriteSetPosition(s_pSuicidometreDeco, 1320.f, 50.f);
+	///
 
 	s_pVillagerCounter = GfxTextSpriteCreate();
 	GfxSpriteSetFilteringEnabled(s_pVillagerCounter, false);
-	GfxTextSpritePrintf(s_pVillagerCounter, "%d villagers alive", TMap::S_GetVillagerCount()); // Get Nbr Villager Alive
 
 	// Initialize suicid gauge sprite
 	TGfxImage * pImage = GfxImageCreate(1, 1);
@@ -77,6 +144,13 @@ void THUD::S_Initialize()
 	s_tSuicideInfo.m_iLastFrameLost = GfxTimeGetMilliseconds();
 }
 
+void THUD::Initialize()
+{
+	for (int i = 0; i < NBR_SPRITE; i++)
+	{
+		GfxSpriteSetPosition(s_pVillagerAliveText[i], 164.f, 100.f + i * 30);
+	}
+}
 void THUD::S_Update()
 {
 	GfxSpriteSetPosition(s_pMouseSprite, float(GfxGetCurrentMouseX()), float(GfxGetCurrentMouseY()));
@@ -91,17 +165,14 @@ void THUD::S_Update()
 		GfxSpriteSetCutout(s_pMouseSprite, 0, 0, 32, 32);
 	}
 
-	S_UpdateVillagerCounter();
 	S_UpdateVillagerSuicideGauge();
 	S_DisplayName();
 
-}
+	// update score;
+	S_SetTextSprite(s_pVillagerAliveText, TMap::S_GetVillagerCount());
+	S_SetTextSprite(s_pVillagerDeadText, s_tSuicideInfo.m_iTotalSuicide);
 
-void THUD::S_UpdateVillagerCounter()
-{
-	GfxTextSpritePrintf(s_pVillagerCounter, "%d villagers alive", TMap::S_GetVillagerCount());	// Get alive villagers count
 }
-
 void THUD::S_UpdateVillagerSuicideGauge()
 {
 	int iSinceLastFrame = GfxTimeGetMilliseconds() - s_tSuicideInfo.m_iLastFrameLost;
@@ -113,8 +184,9 @@ void THUD::S_UpdateVillagerSuicideGauge()
 		s_tSuicideInfo.m_fSuicideMalus = 0.f;
 	}
 
-	GfxSpriteSetScale(s_tSuicideInfo.m_pSuicideGauge, (floorf(s_tSuicideInfo.m_fSuicideMalus)), SUICIDE_GAUGE_SIZE_Y);
+	GfxSpriteSetScale(s_pSuicidometre, (s_tSuicideInfo.m_fSuicideMalus / SUICIDE_MALUS_MAX), 1);
 
+	GfxDbgPrintf("%f \n", s_tSuicideInfo.m_fSuicideMalus / SUICIDE_MALUS_MAX);
 	s_tSuicideInfo.m_iLastFrameLost = GfxTimeGetMilliseconds();
 }
 
@@ -139,17 +211,51 @@ void THUD::S_DisplayName()
 		GfxTextSpritePrintf(pVillager->GetNameSprite(), "%s", pVillager->GetName());
 	}
 }
+
+int THUD::GetNumber(int iNbrVillager, int iColumn)
+{
+	int iNumber = iNbrVillager;
+	for (int i = 0; i < iColumn; i++)
+	{
+		iNumber = iNumber / 10;
+	}
+
+	int iDigit = iNumber % 10;
+
+	return iDigit;
+
+}
+
+
+void THUD::S_SetTextSprite(TGfxSprite ** pSprite, int iNumber)
+{
+	GfxSpriteSetCutout(pSprite[0], (30 * GetNumber(iNumber, 2)), 0, 30, 60);
+	GfxSpriteSetCutout(pSprite[1], (30 * GetNumber(iNumber, 1)), 0, 30, 60);
+	GfxSpriteSetCutout(pSprite[2], (30 * GetNumber(iNumber, 0)), 0, 30, 60);
+}
+
+
 void THUD::S_Render()
 {
-	GfxSpriteRender(s_pMouseSprite);
 
-	GfxTextSpriteRender(s_pVillagerCounter, VILLAGER_COUNTER_POS_X, VILLAGER_COUNTER_POS_Y, EGfxColor_White, 2.0f, false, false);
-	GfxSpriteRender(s_tSuicideInfo.m_pSuicideGauge);
+	for (int i = 0; i < NBR_SPRITE; i++)
+	{
+		GfxSpriteRender(s_pVillagerAliveText[i]);
+		GfxSpriteRender(s_pVillagerDeadText[i]);
+	}
+
+	GfxSpriteRender(s_pSuicidometreDeco);
+	GfxSpriteRender(s_pSuicidometre);
 
 	for (int i = 0; i < TMap::S_GetVillagerCount(); i++)
 	{
 		TVillager * pVillager = TMap::S_GetVillagers()[i];
 		GfxTextSpriteRender(pVillager->GetNameSprite(), pVillager->GetNameLocal().x, pVillager->GetNameLocal().y - pVillager->GetSize().y + 10.0f, EGfxColor_Black, 1.0f, true, true);
 	}
+
+	GfxSpriteRender(s_pVillagerAlive);
+	GfxSpriteRender(s_pVillagerDead);
+
+	GfxSpriteRender(s_pMouseSprite);
 }
 
--- 1415_GameJam/Map.cpp
+++ 1415_GameJam/Map.cpp
@@ -67,7 +67,6 @@ int TMap::s_iLastTimeBirdGeneration = 0;
 void TMap::S_Initialize()
 {
 	THUD::S_Initialize();
-
 	tSpawnLimite = TGfxVec2(TFloor::GetPosition().x - TFloor::GetLeftSize(), TFloor::GetPosition().x + TFloor::GetRightSize());
 
 	s_pParallax = new TParallax();
